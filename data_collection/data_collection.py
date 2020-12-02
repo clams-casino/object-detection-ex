9791eb7e8cf49c66bef36fdd6f56c8dc349b4ac9
Mike <mike.zhang.619@gmail.com>
2020-12-03 00:30:37 +0100

    get bounding boxes and labels; need to add more filtering maybe

--- data_collection/data_collection.py
+++ data_collection/data_collection.py
@@ -5,6 +5,14 @@ import cv2 # TODO remove later
 from agent import PurePursuitPolicy
 from utils import launch_env, seed, makedirs, display_seg_mask, display_img_seg_mask
 
+BACKGROUND = 0
+DUCKIE = 1
+CONE = 2
+TRUCK = 3
+BUS = 4
+
+NN_IMG_SIZE = (224,224)
+
 npz_index = 0
 def save_npz(img, boxes, classes):
     global npz_index
@@ -12,22 +20,60 @@ def save_npz(img, boxes, classes):
         np.savez(f"./data_collection/dataset/{npz_index}.npz", *(img, boxes, classes))
         npz_index += 1
 
-def clean_segmented_image(seg_img):
-    # TODO
-    # Tip: use either of the two display functions found in util.py to ensure that your cleaning produces clean masks
-    # (ie masks akin to the ones from PennFudanPed) before extracting the bounding boxes
 
-    # TODO do not put bounding box and label if the bounding box is too small
+def clean_segmented_image(seg_img, include_background=False):
+    boxes = []
+    labels = []
 
-    # TODO have different bounding box size limits? For example since cones are smaller?
+    # duckies
+    duckie_seg = _get_duckies(seg_img)
+    duckie_seg = cv2.resize(duckie_seg, NN_IMG_SIZE)
+    duckie_bb = _filter_boxes(_get_bounding_boxes(duckie_seg))
+    num_duckies = len(duckie_bb)
+    if num_duckies > 0:
+        boxes.extend(duckie_bb)
+        labels.extend([DUCKIE] * num_duckies)
 
-    # TODO think about metrics for what's a good image to save that can capture a lot of the cases in evaluation?
+    # cones
+    cone_seg = _get_cones(seg_img)
+    cone_seg = cv2.resize(cone_seg, NN_IMG_SIZE)
+    cone_bb = _filter_boxes(_get_bounding_boxes(cone_seg))
+    num_cones = len(cone_bb)
+    if num_cones > 0:
+        boxes.extend(cone_bb)
+        labels.extend([CONE] * num_cones)
 
-    pass
-    # return boxes, classes
+    # trucks
+    truck_seg = _get_trucks(seg_img)
+    truck_seg = cv2.resize(truck_seg, NN_IMG_SIZE)
+    truck_bb = _get_bounding_boxes(truck_seg)
+    num_trucks = len(truck_bb)
+    if num_trucks > 0:
+        boxes.extend(truck_bb)
+        labels.extend([TRUCK] * num_trucks)
+
+    # buses
+    bus_seg = _get_buses(seg_img)
+    bus_seg = cv2.resize(bus_seg, NN_IMG_SIZE)
+    bus_bb = _get_bounding_boxes(bus_seg)
+    num_buses = len(bus_bb)
+    if num_buses > 0:
+        boxes.extend(bus_bb)
+        labels.extend([BUS] * num_buses)
+
+    # get background
+    if include_background:
+        background_seg = _get_background(seg_img)
+        background_seg = cv2.resize(background_seg, NN_IMG_SIZE)
+        background_bb = _get_bounding_boxes(background_seg)
+        # assume always one background segmentation
+        boxes.extend(background_bb)
+        labels.extend([BACKGROUND])
+
+    return boxes, labels
 
 
-def filter_boxes(boxes, min_area=None, min_aspect_ratio=None):
+def _filter_boxes(boxes, min_area=None, min_aspect_ratio=None):
     valid_boxes = []
     for box in boxes:
         w = box[2] - box[0]
@@ -44,7 +90,7 @@ def filter_boxes(boxes, min_area=None, min_aspect_ratio=None):
     return valid_boxes
 
 
-def get_bounding_boxes(binary_seg_img):
+def _get_bounding_boxes(binary_seg_img):
     '''
         Takes a binary segmented image of a single class and returns a list of the bounding boxes
     '''
@@ -52,14 +98,29 @@ def get_bounding_boxes(binary_seg_img):
     boxes = []
     for contour in contours:
             x,y,w,h = cv2.boundingRect(contour)
-            boxes.append( np.array([x, y, x+w, y+h]) )
+            boxes.append( [x, y, x+w, y+h] )
             
     return boxes
 
 
-def draw_bounding_boxes(img, boxes):
-    for box in boxes:
-        img = cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), (255,255,255), thickness=1)
+def _label2color(label):
+    if label == 0:
+        return (255, 0, 255)
+    elif label == 1:
+        return (100, 116, 226)
+    elif label == 2:
+        return (226, 111, 101)
+    elif label == 3:
+        return (116, 114, 117)
+    elif label == 4:
+        return (216, 171, 15)
+    else:
+        raise('invalid label')
+
+
+def draw_bounding_boxes(img, boxes, labels):
+    for box, label in zip(boxes, labels):
+        img = cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), _label2color(label), thickness=2)
     return img
 
 
@@ -103,14 +164,14 @@ def _get_trucks(seg_img):
 def _get_buses(seg_img):
     return _remove_snow(cv2.inRange(segmented_obs, (216, 171, 15), (216, 171, 15)))
 
+
+
 seed(123)
 environment = launch_env()
 
 policy = PurePursuitPolicy(environment)
 
 MAX_STEPS = 500
-
-NN_IMG_SIZE = (224,224)
 
 while True:
     obs = environment.reset()
@@ -129,47 +190,16 @@ while True:
         # environment.render(segment=int(nb_of_steps / 50) % 2 == 0)
 
 
-        background_seg = _get_background(segmented_obs)
-        obs = cv2.resize(obs, NN_IMG_SIZE)
-        background_seg = cv2.resize(background_seg, NN_IMG_SIZE)
-        background_bb = get_bounding_boxes(background_seg)
-        obs_background_bb = draw_bounding_boxes(obs.copy(), background_bb)
+        boxes, labels = clean_segmented_image(segmented_obs)
+        
+        obs_bounding_boxes = cv2.cvtColor(cv2.resize(obs.copy(), NN_IMG_SIZE), cv2.COLOR_RGB2BGR)
 
-        duckie_seg = _get_duckies(segmented_obs)
-        obs = cv2.resize(obs, NN_IMG_SIZE)
-        duckie_seg = cv2.resize(duckie_seg, NN_IMG_SIZE)
-        duckie_bb = filter_boxes(get_bounding_boxes(duckie_seg))
-        obs_duckie_bb = draw_bounding_boxes(obs.copy(), duckie_bb)
+        obs_bounding_boxes = draw_bounding_boxes(obs_bounding_boxes, boxes, labels)
 
-        cone_seg = _get_cones(segmented_obs)
-        obs = cv2.resize(obs, NN_IMG_SIZE)
-        cone_seg = cv2.resize(cone_seg, NN_IMG_SIZE)
-        cone_bb = filter_boxes(get_bounding_boxes(cone_seg))
-        obs_cone_bb = draw_bounding_boxes(obs.copy(), cone_bb)
-
-        truck_seg = _get_trucks(segmented_obs)
-        obs = cv2.resize(obs, NN_IMG_SIZE)
-        truck_seg = cv2.resize(truck_seg, NN_IMG_SIZE)
-        truck_bb = get_bounding_boxes(truck_seg)
-        obs_truck_bb = draw_bounding_boxes(obs.copy(), truck_bb)
-
-        bus_seg = _get_buses(segmented_obs)
-        obs = cv2.resize(obs, NN_IMG_SIZE)
-        bus_seg = cv2.resize(bus_seg, NN_IMG_SIZE)
-        bus_bb = get_bounding_boxes(bus_seg)
-        obs_bus_bb = draw_bounding_boxes(obs.copy(), bus_bb)
-
-        obs_bounding_boxes = obs_background_bb
-        class_seg = background_seg
-
-        side_by_side = np.hstack((obs_bounding_boxes, cv2.applyColorMap(class_seg, cv2.COLORMAP_SPRING)))
-        cv2.imshow('segmented_class',side_by_side)
-        # cv2.imshow('duckie', duckie_seg)
+        cv2.imshow('bounding boxes',obs_bounding_boxes)
         cv2.waitKey(1)
 
 
-
-        # TODO boxes, classes = clean_segmented_image(segmented_obs)
 
         # TODO maybe save every other images (or every nth) to avoid too many basically repeat images?
 
