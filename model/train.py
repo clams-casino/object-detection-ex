a2e7195a0c440be013d8b09549282a13dad1faac
Mike <mike.zhang.619@gmail.com>
2020-12-06 18:37:16 +0100

    train for total of 7 epochs

--- model/train.py
+++ model/train.py
@@ -89,7 +89,7 @@ def main():
                                                    gamma=0.1)
 
     # let's train it for 10 epochs
-    num_epochs = 10
+    num_epochs = 7
 
     for epoch in range(num_epochs):
         # train for one epoch, printing every 10 iterations
